29215bdc135cbeec01dfbc089ebd399c4a0e53ca
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-10-24 19:20:18 +0900

    백준 17298번 문제(실패)

---
+++ 17298.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+stack<int> s;
+
+int main()
+{
+    cin >> N;
+    
+    while (N--)
+    {
+        int input;
+        cin >> input;
+
+        if (!s.empty() && s.top() < input)
+        {
+
+        }
+        else
+        {
+
+        }
+        
+    }
+
+
+    return 0;
+}
+
