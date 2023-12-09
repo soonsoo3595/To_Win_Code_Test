6c62903679323b95f94661bb913e1453f1804ad9
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-12-10 00:55:13 +0900

    백준 1781번 문제 [골드 2]

---
+++ 1781.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main()
+{
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({ a, b });
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (pq.size() < v[i].first)
+        {
+            pq.push(v[i].second);
+        }
+        else if (pq.size() == v[i].first && pq.top() < v[i].second)
+        {
+            pq.pop();
+            pq.push(v[i].second);
+        }
+    }
+
+    int ret = 0;
+
+    while (pq.size())
+    {
+        ret += pq.top(); pq.pop();
+    }
+
+    cout << ret;
+
+    return 0;
+}
+
