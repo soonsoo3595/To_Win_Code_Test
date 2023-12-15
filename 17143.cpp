df4e3e25f2fb09d949955d7b8c8d8ce4b0e6602c
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2023-12-15 19:38:00 +0900

    백준 17143번 문제[골드 1]

--- 17143.cpp
+++ 17143.cpp
@@ -2,16 +2,18 @@
 
 using namespace std;
 
-int arr[104][104];
+int arr[104][104];  // 격자판, 각 칸은 상어가 몇 마리 있는 지를 의미한다.
 
 int R, C, M;
-int fisher = 0;
+int fisher = 0; // 낚시왕의 현재 열 위치
 
-int dy[] = { -1, 1, 0, 0 };
+// 방향벡터 : 상, 하, 우, 좌, d의 순서대로 표현
+int dy[] = { -1, 1, 0, 0 }; 
 int dx[] = { 0, 0, 1, -1 };
 
-int answer;
+int answer; // 정답 변수
 
+// 상어 구조체
 struct Shark
 {
     int r;
@@ -31,11 +33,19 @@ struct Shark
         CheckIn();
     }
 
+    // 상어의 움직임
     void Move()
     {
         CheckOut();
 
-        for (int i = 0; i < s; i++)
+        // 시간복잡도 때문에 속력 s를 줄여야 한다. 
+        int ss;
+        if (d == 1 || d == 2)
+            ss = s % (R * 2 - 2);
+        else if (d == 3 || d == 4)
+            ss = s % (C * 2 - 2);
+
+        for (int i = 0; i < ss; i++)
         {
             int nr = r + dy[d - 1];
             int nc = c + dx[d - 1];
@@ -61,29 +71,26 @@ struct Shark
         CheckIn();
     }
 
+    // 상어가 현재 위치에 있음을 arr 배열에 표시
     void CheckIn()
     {
         arr[r][c]++;
     }
 
+    // 상어가 현재 위치에서 사라졌음을 표시
     void CheckOut()
     {
         arr[r][c]--;
     }
+
+    // 낚시왕이 상어를 잡았을 때
+    void Catch()
+    {
+        answer += z;
+        CheckOut();
+    }
 };
 vector<Shark> v;
-
-void Print()
-{
-    for (int i = 1; i <= R; i++)
-    {
-        for (int j = 1; j <= C; j++)
-        {
-            cout << arr[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-}
 
 int main()
 {
@@ -99,17 +106,75 @@ int main()
     }
 
     
-    while (fisher < 1)
+    while (fisher <= C)
     {
         fisher++;
+        bool catched = false;
 
+        // 낚시왕이 이동 후 같은 열에 있는 상어를 잡는다
+        for (int i = 1; i <= R; i++)
+        {
+            if (arr[i][fisher] && !catched)
+            {
+                for (int j = 0; j < v.size(); j++)
+                {
+                    if (v[j].r == i && v[j].c == fisher)
+                    {
+                        v[j].Catch();
+                        catched = true;
+                        v.erase(v.begin() + j);
+                        break;
+                    }
+                }
+            }
+        }
+
+        // 상어들이 이동한다.
         for (int i = 0; i < v.size(); i++)
         {
             v[i].Move();
         }
 
+        // 이동 후 같은 칸에 있는 상어 처리
+        for (int i = 1; i <= R; i++)
+        {
+            for (int j = 1; j <= C; j++)
+            {
+                // 한 곳에 상어가 두 마리 이상이면
+                if (arr[i][j] >= 2)
+                {
+                    // vv 벡터는 같은 곳에 있는 상어들의 무게, 무게는 모든 상어마다 다르므로 인덱스로 활용 가능
+                    vector<int> vv;
+
+                    for (int k = 0; k < v.size(); k++)
+                    {
+                        if (v[k].r == i && v[k].c == j)
+                        {
+                            vv.push_back(v[k].z);
+                        }
+                    }
+
+                    sort(vv.begin(), vv.end(), greater<int>());
+
+                    for (int k = 1; k < vv.size(); k++)
+                    {
+                        for (int w = 0; w < v.size(); w++)
+                        {
+                            if (v[w].z == vv[k])
+                            {
+                                v[w].CheckOut();
+                                v.erase(v.begin() + w);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
+    cout << answer;
+
     return 0;
 }
 
