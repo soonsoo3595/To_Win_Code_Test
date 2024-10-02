0b35272e983f465d06e6dfd202623efb867df643
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-10-03 01:30:41 +0900

    백준 1389번 문제 [실버 1]
    BFS 문제

---
+++ 1389.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+
+vector<int> adj[104];
+int visited[104];
+int min_kebbin = INT_MAX;
+vector<int> answer;
+
+int main()
+{
+    cin >> N >> M;
+
+    while (M--)
+    {
+        int A, B;
+        cin >> A >> B;
+
+        adj[A].push_back(B);
+        adj[B].push_back(A);
+    }
+
+
+    for (int i = 1; i <= N; i++)
+    {
+        queue<int> q;
+        q.push(i);
+        visited[i] = 1;
+
+        while (!q.empty())
+        {
+            int here = q.front();
+            q.pop();
+
+            for (int there : adj[here])
+            {
+                if (visited[there])
+                    continue;
+
+                q.push(there);
+                visited[there] = visited[here] + 1;
+            }
+        }
+
+        int kebbin = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            kebbin += (visited[i] - 1);
+        }
+
+        if (kebbin < min_kebbin)
+        {
+            min_kebbin = kebbin;
+            answer.clear();
+            answer.push_back(i);
+        }
+        else if (kebbin == min_kebbin)
+        {
+            answer.push_back(i);
+        }
+
+        fill(visited, visited + 104, 0);
+    }
+
+    cout << answer[0];
+
+    return 0;
+}
+
