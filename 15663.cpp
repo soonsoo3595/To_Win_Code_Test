b7e898348d77ab380d4dc661b7c4284fbae0f765
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-10-19 14:44:25 +0900

    백준 15663번 문제 [실버 2]
    재귀 이용한 순열
    중복 순열 제거

---
+++ 15663.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> sequence;
+int visited[9];
+
+void Print()
+{
+    for (int num : sequence)
+    {
+        cout << num << ' ';
+    }
+
+    cout << '\n';
+}
+
+void Go(int start, int depth)
+{
+    if (depth == M)
+    {
+        Print();
+        return;
+    }
+
+    int last = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (!visited[i] && last != arr[i])
+        {
+            visited[i] = true;
+            sequence.push_back(arr[i]);
+            last = arr[i];
+            Go(0, depth + 1);
+            visited[i] = false;
+            sequence.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        int input; cin >> input;
+        arr.push_back(input);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    Go(0, 0);
+
+    return 0;
+}
+
