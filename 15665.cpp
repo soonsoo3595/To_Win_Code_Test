b855661e1ccb2a6ef36e524ee805d3ce65a263bd
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-10-19 14:49:15 +0900

    백준 15665번 문제 [실버 2]
    재귀 이용 순열
    중복 허용

---
+++ 15665.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> sequence;
+int visited[9];
+
+void Print()
+{
+    for (int num : sequence)
+    {
+        cout << num << ' ';
+    }
+
+    cout << '\n';
+}
+
+void Go(int start, int depth)
+{
+    if (depth == M)
+    {
+        Print();
+        return;
+    }
+
+    int last = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (last != arr[i])
+        {
+            sequence.push_back(arr[i]);
+            last = arr[i];
+            Go(0, depth + 1);
+            sequence.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        int input; cin >> input;
+        arr.push_back(input);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    Go(0, 0);
+
+    return 0;
+}
+
