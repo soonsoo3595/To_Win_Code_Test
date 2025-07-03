8ffa03ee6633f9e576aa7c2bc116b26938f99d5f
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2025-07-04 01:28:09 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -2,36 +2,39 @@
 
 using namespace std;
 
-// nCr
-long long dp[31][31];
+int T, N, M;
 
-int main()
+void Input()
 {
-    int T;
-    cin >> T;
-
-    for (int i = 1; i <= 30; i++)
-    {
-        dp[i][1] = i;
-        dp[i][i] = 1;
-    }
-
-    for (int i = 3; i <= 30; i++)
-    {
-        for (int j = 2; j < i; j++)
-        {
-            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-        }
-    }
-
-    while (T--)
-    {
-        int N, M;
-        cin >> N >> M;
-
-        cout << dp[M][N] << '\n';
-    }
-
-    return 0;
+	cin >> T;
 }
 
+void Solve()
+{
+	while (T--)
+	{
+		cin >> N >> M;
+
+		long long answer = 1;
+		int j = 1;
+
+		for (int i = M; i > M - N; i--)
+		{
+			answer *= i;
+			answer /= j++;
+		}
+
+		cout << answer << '\n';
+	}
+}
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	Input();
+	Solve();
+
+	return 0;
+}
